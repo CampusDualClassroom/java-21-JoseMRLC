4e746e5fe9a6386eb64386d8d3e4fd70ffa4d172
JoseMRLC <josemriveiroticbgmail.com>
2024-09-27 11:03:27 +0200

    ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise21.java
+++ src/main/java/com/campusdual/classroom/Exercise21.java
@@ -7,24 +7,60 @@ import java.util.TreeSet;
 public class Exercise21 {
 
     public static Set<String> createHashSet() {
+        Set<String> hashSet = new HashSet<>();
 
+        hashSet.add("ELEMENT KPRBC");
+        hashSet.add("ELEMENT YPBTM");
+        hashSet.add("ELEMENT AADXU");
+        hashSet.add("ELEMENT RXCGJ");
+        hashSet.add("ELEMENT WYMVD");
+        hashSet.add("ELEMENT WFGEJ");
+        hashSet.add("ELEMENT TYGBS");
+        hashSet.add("ELEMENT MAPTK");
+        hashSet.add("ELEMENT GJXVE");
+        hashSet.add("ELEMENT BAFGL");
+
+        return hashSet;
     }
 
     public static Set<String> createTreeSet() {
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add("ELEMENT KPRBC");
+        treeSet.add("ELEMENT YPBTM");
+        treeSet.add("ELEMENT AADXU");
+        treeSet.add("ELEMENT RXCGJ");
+        treeSet.add("ELEMENT WYMVD");
+        treeSet.add("ELEMENT WFGEJ");
+        treeSet.add("ELEMENT TYGBS");
+        treeSet.add("ELEMENT MAPTK");
+        treeSet.add("ELEMENT GJXVE");
+        treeSet.add("ELEMENT BAFGL");
 
+        return treeSet;
     }
 
     private static void printSet(Set<String> customSet) {
-
+        for (String element : customSet) {
+            System.out.println(element); // Imprimir cada elemento
+        }
     }
 
 
     public static boolean addElementToSet(Set<String> set, String elementToAdd) {
-
+        return set.add(elementToAdd);
     }
 
     public static void main(String[] args) {
-
+        Set<String> hashSet = Exercise21.createHashSet();
+        boolean addedToHashSet = Exercise21.addElementToSet(hashSet, "ELEMENT AAA");
+        System.out.println("Elemento añadido al HashSet: " + addedToHashSet);
+        System.out.println("Contenido del HashSet:");
+        Exercise21.printSet(hashSet);
+        Set<String> treeSet = Exercise21.createTreeSet();
+        boolean addedToTreeSet = Exercise21.addElementToSet(treeSet, "ELEMENT AAA");
+        System.out.println("Elemento añadido al TreeSet: " + addedToTreeSet);
+        System.out.println("Contenido del TreeSet:");
+        Exercise21.printSet(treeSet);
     }
 
 }
